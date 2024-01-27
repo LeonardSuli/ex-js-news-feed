f3db878585d1fa06fecc5b8b3fb24ff7f857004d
LeonardSuli <leonardsuli1991@gmail.com>
2024-01-27 14:23:56 +0100

    fixed all milestone

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-
-
 // Step 1: struttura dei dati
 // Partendo dai dati forniti crea le strutture dati necessarie sfruttando array 
 // e oggetti facendo attenzione agli attributi che caratterizzano ciascuna news.
@@ -64,20 +62,15 @@ const cards = [
     }
 ];
 
-// console.log(cards);
 
 // Card bookmarkate
 const bookmarked = [1];  
-console.log(bookmarked);
 
+
+// Variabili
 let filteredCards = cards;
-
 let checkedCards = cards;
-  
-// Step 2 - Stampa dei dati in pagina
-// Prendendo come riferimento il layout di esempio presente nell'HTML 
-// stampa in pagina le news del nostro feed utilizzando JavaScript.
-  
+   
 
 // Seleziono l'elemento della DOM dove aggiungo tutte le card e la salvo in una variabile
 const cardsEl = document.querySelector('.cards');
@@ -89,26 +82,13 @@ const selectEl = document.getElementById('tag_type');
 const checkEl = document.getElementById('checked');
 
 
-
-
 // Funzione che rende una lista di card dentro la DOM
 renderCards(cards, cardsEl);
-
-
-// Step 3: filtri
-// Crea l’interfaccia dei filtri utilizzando tag di input appropriati. 
-// Recupera in JavaScript i valori selezionati dall’utente da utilizzare 
-// nel codice per le logiche di filtraggio gli elementi.
-
-
-
 
 
 // Aggiungo un event listener con l'evento change per la select
 selectEl.addEventListener('change', function(e){
 
-  // console.log(e.target.value);
-  
   // Filtro le carte in base al tag selezionato
   const filteredCards = cards.filter(card => {
 
@@ -116,29 +96,26 @@ selectEl.addEventListener('change', function(e){
     for (let i = 0; i < card.tags.length; i++) {
       
       if (card.tags[i] === e.target.value || e.target.value === 'all') {
+
         return true;
+
       }
       
     }
     
   });
 
-  // console.log(filteredCards);
-  
-  joinCards = filteredCards.filter(value => checkedCards.includes(value))
 
+  joinCards = filteredCards.filter(value => checkedCards.includes(value));
 
-  manageCardRendering(joinCards)
-
-  
+  // Funzione per gestire l'esecuzione delle cards della select
+  manageCardRendering(joinCards);
 
 });
 
 
 // Ottengo l'unicità dei tipi di tag
 const tagsList = new Set(cards.flatMap(card => card.tags));
-
-// console.log(tagsList);
 
 
 // Funzione che restituisce le options dinamicamente
@@ -150,27 +127,27 @@ checkEl.addEventListener('change', function(e){
 
   if(!e.target.checked){
 
-    checkedCards = cards
+    checkedCards = cards;
 
-    manageCardRendering(filteredCards)
+    manageCardRendering(filteredCards);
 
     return
+
   }
 
 
   checkedCards = cards.filter(card => {
 
-    return bookmarked.includes(card.id)
+    return bookmarked.includes(card.id);
 
   })
 
 
-  joinCards = checkedCards.filter(value => filteredCards.includes(value))
+  joinCards = checkedCards.filter(value => filteredCards.includes(value));
 
+  // Funzione per gestire l'esecuzione delle cards della checkbox
+  manageCardRendering(joinCards);
 
-  manageCardRendering(joinCards)
-
-  // console.log(e.target.checked);
 })
 
 
@@ -195,31 +172,8 @@ function manageCardRendering(cards){
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-// Step 4: bookmark
-// 1-Crea su ogni componente News un pulsante per il salvataggio della News.
-// 2-Se clicchiamo l’icona bookmark, cambiamo l’aspetto dell’icona (es. da vuota a piena) 
-//   e aggiungiamo l’id della News nell’array degli id delle news salvate.
-// 3-L’id della news è un dato “nascosto” che vorrai inserire in pagina per recuperarlo in seguito
-//   al click dell’icona bookmark. Per farlo dovresti utilizzare un data-attribute.
-// 4-In fase di stampa dell’elenco di news dovrai controllare se la news è salvata o meno 
-//   per poter dare il giusto aspetto all’icona bookmark.
-
-
-
-
 /**
- * Rende tutte le options nella select dinamicamente
+ * Funzione che rende tutte le options nella select dinamicamente
  * @param {Array} optionsList Una lista di stringhe
  * @param {object} selectDomEl L'elemento DOM della select dove appendere tutte le options
  */
@@ -232,20 +186,10 @@ function renderOptions(optionsList, selectDomEl){
     optionEl.value = optionValue;
     optionEl.innerText = optionValue;
     
-    
-    // const capitalizedName = optionValue.charAt(0).toUpperCase() + optionValue.slice(1, optionValue.length).toLowerCase();
-    
-    // optionEl.value = capitalizedName;
-    // optionEl.innerText = capitalizedName;
-    
     selectDomEl.appendChild(optionEl);
     
   })
 }
-
-
-
-
 
 
 /**
@@ -258,79 +202,55 @@ function generateCard(card){
   // Destructuring della card
   const {id, title, content, tags, author, published, image} = card;
 
-  // console.log(id, title, content, tags, author, published, image);
-    
   // Inserito data in formato italiano
-  // card.published = new Date(published).toLocaleDateString('it-IT');
   const date = card.published.split('-').reverse().join('/');
-  // console.log(card.published);
   
   // Ciclo map per creare bottoni ad ogni nuovo tag aggiunto
-  const tagsButtons = tags.map(tag => 
-    `<button class="btn_${tag}">${tag}</button>`).join('');
+  const tagsButtons = tags.map(tag => `<button class="btn_${tag}">${tag}</button>`).join('');
 
     
     // Creato markup della card in HTML con il template literal
-    const cardMarkup = `<div class="card">
-                            <h2>${title}</h2>
-                            <span id="author">pubblicato da ${author}</span>
-                            <span id="published">in data ${date}</span>
-                            <p id="content">${content}</p>
-                            <img src="./assets/img/${image.url}" alt="${image.alt}">
-                            ${tagsButtons}
-                            <div>
-                                <i class="fa-${isBookmarked(card) ? 'solid' : 'regular'} fa-bookmark ${isBookmarked(card) ? 'bookmarked' : '' }" data-id="${id}"></i>
-                            </div>
-                        </div>`;
-    
+  const cardMarkup = `<div class="card">
+                          <h2>${title}</h2>
+                          <span id="author">pubblicato da ${author}</span>
+                          <span id="published">in data ${date}</span>
+                          <p id="content">${content}</p>
+                          <img src="./assets/img/${image.url}" alt="${image.alt}">
+                          ${tagsButtons}
+                          <div>
+                              <i class="fa-${isBookmarked(card) ? 'solid' : 'regular'} fa-bookmark ${isBookmarked(card) ? 'bookmarked' : '' }" data-id="${id}"></i>
+                          </div>
+                      </div>`;
 
-                        console.log(isBookmarked(card));
+  return cardMarkup    
 
-                        // console.log(cardMarkup)
-
-    return cardMarkup                                         
 };
-
-
-
-
-
 
 
 // Funzione per selezionare il bookmark
 function handleCardClicks(e){
-    console.log(e.target.getAttribute('data-id'));
+
+    // console.log(e.target.getAttribute('data-id'));
 
     const iconId = Number(e.target.getAttribute('data-id'))
 
     bookmarked.push(iconId)
 
-    console.log(bookmarked);
-
     e.target.className = `fa-solid fa-bookmark bookmarked`
-
 
 };
 
 
-
-
-// Funzione per sapere se l'elemento è bookmarkato o meno
 /**
- * 
+ * Funzione per sapere se l'elemento è bookmarkato o meno
  * @param {Object} card 
  * @returns {Boolean}
  */
 function isBookmarked(card){
+
   return bookmarked.includes(card.id)
+
 }
-
-
-
-
-
-
-
 
 
 /**
@@ -343,8 +263,6 @@ function renderCards(cardsList, domElement) {
   // Uso il ciclo foreach per iterare le card
   cardsList.forEach(card => {
     
-    // console.log(card);
-    
     // Inserisco la funzione per generare la card col markup
     // e la faccio iterare nel ciclo forEach
     const cardEl = generateCard(card);
@@ -355,45 +273,14 @@ function renderCards(cardsList, domElement) {
     
   })
 
-  // Bookmark
+  // Prendo il Bookmark
   const iconsEl = document.querySelectorAll('.card i');
-  console.log(iconsEl);
 
-
+  // Itero sopra il bookmark cliccati
   iconsEl.forEach(card => {
-    console.log(card);
 
     card.addEventListener('click', handleCardClicks)
 
   })
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
